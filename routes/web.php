48781fd8b6732b8f782a22911930f975e23a503e
Renatowilliam21 <renatowilliam21@gmail.com>
2026-02-22 14:55:06 -0300

    Implementa rotas e controladores do módulo inicial

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\PrincipaController;
+use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PrincipaController::class, 'principal']);
+
+Route::get('/sobre', [SobreNosController::class, 'sobrenos']);
+
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+
+
+
+
+
